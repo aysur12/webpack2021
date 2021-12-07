5c21ecc5f4ffa3416d3aaf791cc7f7d18acad9ba
Aysur <aysursabin@gmail.com>
2021-12-07 13:34:44 +0700

    second configuration of Webpack

--- dist/js/colors-and-type.b2393b7329c68e184c4c.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("Hello World!")})();
-//# sourceMappingURL=colors-and-type.b2393b7329c68e184c4c.js.map
--- dist/js/form-elements.bcdfe43977d69bc332ec.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("Hello Forms!")})();
-//# sourceMappingURL=form-elements.bcdfe43977d69bc332ec.js.map
