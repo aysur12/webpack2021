de60282785305b5afa229aaa4495fdbad5782ed3
Aysur <aysursabin@gmail.com>
2022-01-03 16:31:54 +0700

    refactoring style of component 'pallete'

--- src/pages/colors-and-type/index.js
+++ src/pages/colors-and-type/index.js
@@ -1,8 +1,9 @@
-import '../../styles/settings.scss';
+import '../../index';
 import '../../layouts/ui-kit-layout';
 
 import '../../components/logo';
 import '../../components/pallete';
+import '../../components/type';
 
 import './colors-and-type.scss';
 
