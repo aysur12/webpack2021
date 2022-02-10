7dbb9ac5c279e882cb1c0606ffc9826c24a2e6b2
Aysur <aysursabin@gmail.com>
2022-02-10 23:40:05 +0700

    add block 'checkbox buttons'

--- src/components/masked-text-field/index.js
+++
@@ -1,1 +0,0 @@
-import './masked-text-field.scss';
--- src/pages/form-elements/index.js
+++ src/pages/form-elements/index.js
@@ -2,7 +2,8 @@ import '../../components/title-input';
 import '../../components/desc-state-input';
 import '../../components/text-field';
 import '../../components/dropdown-common-value';
-import '../../components/masked-text-field';
+
+import '../../components/checkbox';
 import '../../index';
 
 import './form-elements.scss';
