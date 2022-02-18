708898987753bc3bdb911b39f427d1c2438ac7a9
Aysur Kuchkashev <aysursabin@gmail.com>
2022-02-18 22:18:08 +0700

    рефакторинг компонента 'like button'

--- src/components/like-button/index.js
+++ src/components/like-button/index.js
@@ -1,20 +1,19 @@
 import './like-button.scss';
 
-const likeButton = document.querySelectorAll('.like-button');
-const likeIcon = document.querySelector('.like-button__icon');
-const likeCount = document.querySelector('.like-button__count');
+const likeButton = document.querySelectorAll('.js-like-button');
 
 likeButton.forEach((button) => {
   button.addEventListener('click', (e) => {
-    likeIcon.innerHTML === (likeIcon.innerHTML === 'favorite_border') ? likeIcon.innerHTML = 'favorite' : likeIcon.innerHTML = 'favorite_border';
-    likeCount.classList.toggle('like-button__count_active');
     button.classList.toggle('like-button_active');
+    button.children[1].classList.toggle('like-button__count_active');
 
     let number = button.children[1].textContent;
     if (button.classList.contains('like-button_active')) {
-        button.children[1].textContent = parseInt(number) + 1;
+      button.children[0].textContent = 'favorite';
+      button.children[1].textContent = parseInt(number) + 1;
     } else {
-        button.children[1].textContent = parseInt(number) - 1;
+      button.children[0].textContent = 'favorite_border';
+      button.children[1].textContent = parseInt(number) - 1;
     }
   });
 });
