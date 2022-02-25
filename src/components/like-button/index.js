338222ce7320ce8b32836e79ae9a3fd70e58e46e
Aysur Kuchkashev <aysursabin@gmail.com>
2022-02-25 16:42:39 +0700

    refactoring 2 with  eslint

--- src/components/like-button/index.js
+++ src/components/like-button/index.js
@@ -2,7 +2,8 @@ import './like-button.scss';
 
 const likeButton = document.querySelectorAll('.js-like-button');
 
-likeButton.forEach((button) => {
+likeButton.forEach((buttonLike) => {
+  const button = buttonLike;
   button.addEventListener('click', (e) => {
     button.classList.toggle('like-button_active');
     button.children[1].classList.toggle('like-button__count_active');
@@ -10,10 +11,10 @@ likeButton.forEach((button) => {
     const number = button.children[1].textContent;
     if (button.classList.contains('like-button_active')) {
       button.children[0].textContent = 'favorite';
-      button.children[1].textContent = parseInt(number) + 1;
+      button.children[1].textContent = parseInt(number, 10) + 1;
     } else {
       button.children[0].textContent = 'favorite_border';
-      button.children[1].textContent = parseInt(number) - 1;
+      button.children[1].textContent = parseInt(number, 10) - 1;
     }
   });
 });
