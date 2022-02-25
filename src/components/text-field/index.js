74d1fa8b78ebf116f48fecd23c9fbd26d0b27680
Aysur Kuchkashev <aysursabin@gmail.com>
2022-02-25 16:46:02 +0700

    after  eslint

--- src/components/text-field/index.js
+++ src/components/text-field/index.js
@@ -2,9 +2,9 @@ import * as jQuery from 'jquery';
 import 'jquery.maskedinput/src/jquery.maskedinput';
 import 'material-icons/iconfont/material-icons.css';
 
-import '../../components/arrow';
+import '../arrow';
 import './text-field.scss';
 
-jQuery(function($) {
-  $(".text-field__input_masked").mask("99.99.9999");
-}); 
+jQuery(($) => {
+  $('.text-field__input_masked').mask('99.99.9999');
+});
