44458ca32f4643a61ffb6dce10477f9ed60de675
Aysur Kuchkashev <aysursabin@gmail.com>
2022-03-01 00:23:12 +0700

    add components 'checkbox-list' and 'benefit'

--- src/pages/form-elements/index.js
+++ src/pages/form-elements/index.js
@@ -3,6 +3,9 @@ import '../../components/desc-state-input';
 import '../../components/text-field';
 import '../../components/dropdown-common-value';
 
+import '../../components/checkbox-list';
+import '../../components/benefit';
+
 import '../../components/checkbox';
 import '../../components/radio';
 import '../../components/toggle';
