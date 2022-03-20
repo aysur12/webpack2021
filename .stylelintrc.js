1c0a23ba6a4af941e7ac1ffaea714a2787218c0b
Aysur <aysursabin@gmail.com>
2022-03-20 19:21:29 +0700

    refactoring with stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -12,6 +12,7 @@ module.exports = {
       "border-in-box-model": false,
       "empty-line-between-groups": false,
     }],
+    "prettier/prettier": [true, {"singleQuote": true}],
     "string-quotes": "single",
     "color-function-notation": "legacy",
     "at-rule-no-unknown" : null,
