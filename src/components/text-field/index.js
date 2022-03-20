b0cd56d259fed667fa33a84f80dc0204617aaf04
Aysur <aysursabin@gmail.com>
2022-03-20 21:45:25 +0700

    import correction through *

--- src/components/text-field/index.js
+++ src/components/text-field/index.js
@@ -1,4 +1,4 @@
-import * as jQuery from 'jquery';
+import jQuery from 'jquery';
 import 'jquery.maskedinput/src/jquery.maskedinput';
 import 'material-icons/iconfont/material-icons.css';
 
@@ -8,3 +8,11 @@ import './text-field.scss';
 jQuery(($) => {
   $('.text-field__input_masked').mask('99.99.9999');
 });
+
+
+/*
+const $inputMasked = $('.text-field__input_masked');
+$inputMasked.mask('99.99.9999');
+
+js-text-field__input_masked
+*/
